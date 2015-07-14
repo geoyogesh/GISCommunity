0ae60a4b120c6d7d43e644c99604ab535cdd0d58
geoyogesh <geoyogesh@gmail.com>
2015-07-14 00:23:41 -0400

    added pages to website

--- src/GISCommunity/GISCommunity/Global.asax.cs
+++ src/GISCommunity/GISCommunity/Global.asax.cs
@@ -19,5 +19,33 @@ namespace GISCommunity
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
+
+        //Disabling Http
+        /*Working but Url is not changing*/
+        /*
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"]
+            + HttpContext.Current.Request.RawUrl);
+            }
+        }
+         
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+          switch (Request.Url.Scheme)
+          {
+            case "https":
+              Response.AddHeader("Strict-Transport-Security", "max-age=300");
+              break;
+            case "http":
+              var path = "https://" + Request.Url.Host + Request.Url.PathAndQuery;
+              Response.Status = "301 Moved Permanently";
+              Response.AddHeader("Location", path);
+              break;
+          }
+        }
+         * */
     }
 }
